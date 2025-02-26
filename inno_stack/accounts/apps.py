adc0c614b899612e2178c30768e4b1a67619793d
Rojin Saji <rojinsaji@Rojins-MacBook-Air.local>
2025-02-26 20:35:32 +0530

    removed apps.py file temporarily for migration

--- inno_stack/accounts/apps.py
+++ inno_stack/accounts/apps.py
@@ -1,28 +1,28 @@
-from django.apps import AppConfig
-from django.db.models.signals import post_migrate
+# from django.apps import AppConfig
+# from django.db.models.signals import post_migrate
 
 
-class AccountsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accounts'
+# class AccountsConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'accounts'
 
 
-    def ready(self):
-        from django_celery_beat.models import PeriodicTask, IntervalSchedule
-        import json
+#     def ready(self):
+#         from django_celery_beat.models import PeriodicTask, IntervalSchedule
+#         import json
         
-        schedule, created = IntervalSchedule.objects.get_or_create(
-            every=23,
-            period=IntervalSchedule.HOURS,
-        )
+#         schedule, created = IntervalSchedule.objects.get_or_create(
+#             every=23,
+#             period=IntervalSchedule.HOURS,
+#         )
         
-        PeriodicTask.objects.get_or_create(
-            name='Refresh access tokens',
-            task='accounts.tasks.refresh_access_tokens',
-            interval=schedule,
-            kwargs=json.dumps({}),
-            expires=None,
-        )
+#         PeriodicTask.objects.get_or_create(
+#             name='Refresh access tokens',
+#             task='accounts.tasks.refresh_access_tokens',
+#             interval=schedule,
+#             kwargs=json.dumps({}),
+#             expires=None,
+#         )
 
 
 
