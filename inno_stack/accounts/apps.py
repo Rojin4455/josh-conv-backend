dc527722e9fdfd9cba52ebbef7576e327825cfe6
Rojin Saji <rojinsaji@Rojins-MacBook-Air.local>
2025-02-27 17:42:34 +0530

    added post_migrate in apps.py

--- inno_stack/accounts/apps.py
+++ inno_stack/accounts/apps.py
@@ -1,30 +1,27 @@
-# from django.apps import AppConfig
-# from django.db.models.signals import post_migrate
+from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
-# class AccountsConfig(AppConfig):
-#     default_auto_field = 'django.db.models.BigAutoField'
-#     name = 'accounts'
+class AccountsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'accounts'
 
+    def ready(self):
+        from django_celery_beat.models import PeriodicTask, IntervalSchedule
+        import json
 
-#     def ready(self):
-#         from django_celery_beat.models import PeriodicTask, IntervalSchedule
-#         import json
-        
-#         schedule, created = IntervalSchedule.objects.get_or_create(
-#             every=23,
-#             period=IntervalSchedule.HOURS,
-#         )
-        
-#         PeriodicTask.objects.get_or_create(
-#             name='Refresh access tokens',
-#             task='accounts.tasks.refresh_access_tokens',
-#             interval=schedule,
-#             kwargs=json.dumps({}),
-#             expires=None,
-#         )
+        def create_periodic_task(sender, **kwargs):
+            schedule, created = IntervalSchedule.objects.get_or_create(
+                every=23,
+                period=IntervalSchedule.HOURS,
+            )
 
+            PeriodicTask.objects.get_or_create(
+                name='Refresh access tokens',
+                task='accounts.tasks.refresh_access_tokens',
+                interval=schedule,
+                kwargs=json.dumps({}),
+                expires=None,
+            )
 
-
-
-        
+        post_migrate.connect(create_periodic_task, sender=self)
