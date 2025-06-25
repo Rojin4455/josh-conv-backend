80e8f7331bf78e7df1c1686f885f498b8ca859d4
Rojin4455 <rojinsaji35@gmail.com>
2025-06-26 02:25:43 +0530

    added api in prefix

--- inno_stack/inno_stack/urls.py
+++ inno_stack/inno_stack/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls'))
+    path('api/admin/', admin.site.urls),
+    path('api/accounts/', include('accounts.urls'))
 ]
